aaaec039ecd397261984f0e2e6abac09cd3ad6dd
owusuvictorjnr <owusuvictor80@gmail.com>
2024-01-09 16:56:30 +0000

    rendering product items at homepage

---
+++ components/products/ProductItem.tsx
@@ -0,0 +1,34 @@
+import { Product } from '@/lib/models/ProductModel'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function ProductItem({ product }: { product: Product }) {
+  return (
+    <div className="card bg-base-300 mb-4">
+      <figure>
+        <Link href={`/product/${product.slug}`}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={300}
+            height={300}
+            className="h-64 bg-cover w-full"
+          />
+        </Link>
+      </figure>
+
+      <div className="card-body">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="card-title font-normal">{product.name}</h2>
+        </Link>
+
+        <p className="mb-2">{product.brand}</p>
+
+        <div className="card-actions flex items-center justify-between">
+          <span className="text-2xl">&#x20B5; {product.price}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
