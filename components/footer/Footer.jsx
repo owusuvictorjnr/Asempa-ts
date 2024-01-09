0012233ddf9d999ef06f140394f179283835a253
owusuvictorjnr <owusuvictor80@gmail.com>
2024-01-09 15:52:01 +0000

    footter page

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer footer-center p-4 bg-base-300 text-base-content">
+      <p className='capitalize'>copyright &copy; 2023 all rights reserved by asempa brand</p>
+    </div>
+  )
+}
+
+export default Footer
