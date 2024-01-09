0d941dac7ddbe3d66c8fd5de1549f131a03cbf6d
owusuvictorjnr <owusuvictor80@gmail.com>
2024-01-09 16:56:42 +0000

    smaple data

---
+++ lib/data.ts
@@ -0,0 +1,99 @@
+const data = {
+  products: [
+    {
+      name: 'T-shirt',
+      slug: 't-shirt',
+      category: 'men shirt',
+      image: '/images/pic1.jpg',
+      price: 100,
+      brand: 'african print',
+      rating: 4.5,
+      numReviews: 7,
+      countInStock: 20,
+      description: 'Product description',
+      isFeatured: true,
+      banner: '/images/banner1.jpg',
+    },
+
+
+    {
+        name: 'Fit shirt',
+        slug: 'fit shirt',
+        category: 'men shirt',
+        image: '/images/pic2.jpg',
+        price: 100,
+        brand: 'african print',
+        rating: 4.5,
+        numReviews: 7,
+        countInStock: 10,
+        description: 'Product description',
+        isFeatured: true,
+        banner: '/images/banner2.jpg',
+      },
+
+      {
+        name: 'Pants',
+        slug: ' pants',
+        category: 'women shirt',
+        image: '/images/pic3.jpg',
+        price: 100,
+        brand: 'african print',
+        rating: 3.5,
+        numReviews: 7,
+        countInStock: 15,
+        description: 'Product description',
+        isFeatured: true,
+        banner: '/images/banner3.jpg',
+      },
+
+
+      {
+        name: 'Overall',
+        slug: 'overall',
+        category: 'unisex',
+        image: '/images/pic4.jpg',
+        price: 100,
+        brand: 'african print',
+        rating: 4.0,
+        numReviews: 7,
+        countInStock: 5,
+        description: 'Product description',
+        isFeatured: true,
+        banner: '/images/banner4.jpg',
+      },
+
+
+      {
+        name: 'Jeans',
+        slug: 'jeans',
+        category: 'women shirt',
+        image: '/images/pic5.jpg',
+        price: 100,
+        brand: 'african print',
+        rating: 4.4,
+        numReviews: 7,
+        countInStock: 8,
+        description: 'Product description',
+        isFeatured: true,
+        banner: '/images/banner5.jpg',
+      },
+
+
+      {
+        name: 'Bag',
+        slug: 'back pack',
+        category: 'unisex',
+        image: '/images/pic6.jpg',
+        price: 100,
+        brand: 'african print',
+        rating: 4.5,
+        numReviews: 7,
+        countInStock: 20,
+        description: 'Product description',
+        isFeatured: true,
+        banner: '/images/banne6.jpg',
+      },
+  ],
+}
+
+export default data
