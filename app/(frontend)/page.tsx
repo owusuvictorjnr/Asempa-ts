680de42cb4425671115150ec3cfb022a33f0f452
owusuvictorjnr <owusuvictor80@gmail.com>
2024-01-09 16:55:56 +0000

    product items

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,9 +1,16 @@
-
+import ProductItem from '@/components/products/ProductItem'
+import data from '@/lib/data'
 
 export default function Home() {
   return (
     <>
       <h2 className="py-2 text-2xl capitalize">latest products</h2>
+
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem key={product.slug} product={product} />
+        ))}
+      </div>
     </>
   )
 }
