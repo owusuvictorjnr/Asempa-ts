a370f5eb3d2e8b6d2d7ad6c2f0e9947be465780d
owusuvictorjnr <owusuvictor80@gmail.com>
2024-01-09 18:07:17 +0000

    TODO: add other features

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -4,12 +4,19 @@ import data from '@/lib/data'
 export default function Home() {
   return (
     <>
-      <h2 className="py-2 text-2xl capitalize">latest products</h2>
+      <div className="">
+        <h2 className="py-2 text-2xl capitalize">latest products</h2>
 
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {data.products.map((product) => (
-          <ProductItem key={product.slug} product={product} />
-        ))}
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {data.products.map((product) => (
+            <ProductItem key={product.slug} product={product} />
+          ))}
+        </div>
+
+        {/* TODO: other products will go here */}
+        <div className="">
+          <h2 className="py-2 text-2xl capitalize">featured products</h2>
+        </div>
       </div>
     </>
   )
