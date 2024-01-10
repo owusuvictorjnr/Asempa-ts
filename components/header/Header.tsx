5762ac96a68493c7df9821abeac2066dbb52631b
owusuvictorjnr <owusuvictor80@gmail.com>
2024-01-10 07:10:40 +0000

    import cart and login menu

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import Menu from './Menu'
 
 const Header = () => {
   return (
@@ -10,25 +11,7 @@ const Header = () => {
             <h1 className="capitalize">asempa brand</h1>
           </Link>
 
-          <ul className="flex">
-            <li>
-              <Link
-                href="/cart"
-                className="btn btn-ghost rounded-btn capitalize"
-              >
-                <h4 className="capitalize">cart</h4>
-              </Link>
-            </li>
-
-            <li>
-              <Link
-                href="/signin"
-                className="btn btn-ghost rounded-btn capitalize"
-              >
-                <h4 className="capitalize">signin</h4>
-              </Link>
-            </li>
-          </ul>
+          <Menu />
         </div>
       </nav>
     </header>
