4725df929dcd15a740f8d7d70cd4a9cfc17336c3
owusuvictorjnr <owusuvictor80@gmail.com>
2024-01-10 07:10:13 +0000

    cart accumulator and login

---
+++ components/header/Menu.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import useCartService from '@/lib/hooks/useCartStore'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+const Menu = () => {
+  const { items } = useCartService()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    <div>
+      <ul className="flex items-stretch">
+        <li>
+          <Link className="btn btn-ghost rounded-btn capitalize" href="/cart">
+            Cart
+            {mounted && items.length != 0 && (
+              <div className="badge badge-secondary">
+                {items.reduce((a, c) => a + c.qty, 0)}{' '}
+              </div>
+            )}
+          </Link>
+        </li>
+
+        <li>
+          <button
+            className="btn btn-ghost rounded-btn capitalize"
+            type="button"
+          >
+            Sigin in
+          </button>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Menu
