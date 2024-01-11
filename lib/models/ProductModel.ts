be2d5173f09036e3d58161eceefbe0b5e2f3a720
owusuvictorjnr <owusuvictor80@gmail.com>
2024-01-11 16:00:14 +0000

    add backend productSchema

--- lib/models/ProductModel.ts
+++ lib/models/ProductModel.ts
@@ -1,3 +1,32 @@
+import mongoose from 'mongoose'
+
+// Backend Schema
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
+    brand: { type: String, required: true },
+    price: { type: String, required: true },
+    rating: { type: String, required: true, default: 0 },
+    numReviews: { type: Number, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+    description: { type: String, required: true },
+    isFeatured: { type: Boolean, required: true },
+    banner: String,
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const ProductModel =
+  mongoose.models.Product || mongoose.model('Product', productSchema)
+
+export default ProductModel
+
+// Frontend Scehema
 export type Product = {
   _id?: string
   name: string
